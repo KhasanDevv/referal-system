f33bc55968d43260a65927826ba5976e7ff8288a
khasan0102 <khasanabdukarimov1@gmail.com>
2022-12-09 19:35:17 +0500

    feat: add market user model controller

--- src/market-users/dto/create-market-user.dto.ts
+++ src/market-users/dto/create-market-user.dto.ts
@@ -1,12 +1,17 @@
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsOptional } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateMarketUserDto {
   @ApiProperty()
   @IsNumber()
-  marketId: string;
+  marketId: number;
 
   @ApiProperty()
   @IsNumber()
-  userId: string;
+  userId: number;
+
+  @ApiProperty()
+  @IsNumber()
+  @IsOptional()
+  referralId: number;
 }
--- src/market-users/market-users.module.ts
+++ src/market-users/market-users.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { MarketUsersService } from './market-users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MarketUserEntity } from './market-user.entity';
+import { MarketUsersController } from './market-users.controller';
+import { MarketEntity } from '../market/market.entity';
+import { UserEntity } from '../users/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MarketUserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([MarketUserEntity, MarketEntity, UserEntity]),
+  ],
   providers: [MarketUsersService],
+  controllers: [MarketUsersController],
 })
 export class MarketUsersModule {}
