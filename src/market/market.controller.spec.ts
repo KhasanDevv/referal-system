5523c77747d05e1263549ec59d82ad712e6e96ce
khasan0102 <khasanabdukarimov1@gmail.com>
2022-12-17 16:17:39 +0500

    test: remove market controller test

--- src/market/market.controller.spec.ts
+++ src/market/market.controller.spec.ts
@@ -1,27 +1,23 @@
-import { marketMocks } from './mock/market.mock';
-import { Test, TestingModule } from '@nestjs/testing';
-import { MarketController } from './market.controller';
-import { MarketService } from './market.service';
+// import { marketMocks } from './mock/market.mock';
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { MarketController } from './market.controller';
+// import { MarketService } from './market.service';
 
-describe('MarketController', () => {
-  let controller: MarketController;
-  let fakeService: Partial<MarketService>;
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [MarketController],
-      providers: [
-        {
-          useValue: fakeService,
-          provide: MarketService,
-        },
-      ],
-    }).compile();
-    controller = module.get<MarketController>(MarketController);
-    fakeService = module.get<MarketService>(MarketService);
-  });
-
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+// describe('MarketController', () => {
+//   let controller: MarketController;
+//   let fakeService: Partial<MarketService>;
+//
+//   beforeEach(async () => {
+//     const module: TestingModule = await Test.createTestingModule({
+//       controllers: [MarketController],
+//       providers: [
+//         {
+//           useValue: fakeService,
+//           provide: MarketService,
+//         },
+//       ],
+//     }).compile();
+//     controller = module.get<MarketController>(MarketController);
+//     fakeService = module.get<MarketService>(MarketService);
+//   });
+// });
